37d22ad106ad82fe3bf2036918d3e96fdc836fd3
kellik25 <147672243+kellik25@users.noreply.github.com>
2024-02-10 19:10:06 -0500

    Update CEEO.py

--- micropython/CEEO.py
+++ micropython/CEEO.py
@@ -1,12 +1,29 @@
-import Buttons
+from Buttons import Buttons
 from Sound import Sound
 import Lights
+import Help
 
-#l = Lights.Lights()
+l = Lights.Lights()
+#l.display_on() what does this do does not output anything
+#l.display_update()
+#l.start()
+#l.timer_init()
+#l.power_led(0,0,0)
+#l.ble_led(0,0,0)
+#l.battery_led(0,0,0)
+#l.matrix_led(2,1)
+
 #l.full_test()
 #l.power_led(0,0,1)
-b = Buttons.Buttons()
-b.read()
+
+b = Buttons()
+measured_value = 3000
+print(b.search(measured_value))
+print(b.read())
+
+#find way to get rid of the Help.help so that it is just help
+Help.help("read")
+
 #s = Sound.Sound()
-#s = Sound()
-#s.play(500,2)
+s = Sound()
+s.play(500,2)
