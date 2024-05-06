872a86fff995d142f062c4f8c874acbdacad9d33
Kelli Kowalick <147672243+kellik25@users.noreply.github.com>
2024-05-05 22:51:04 -0400

    Update Lights.py
    updated docstrings. NOTE: comment on battery_led - not sure if this is an error with my block of with the code in general.

--- micropython/Lights.py
+++ micropython/Lights.py
@@ -4,7 +4,13 @@ from array import array
 
 class Lights():
     '''
-    This class controls the light functions of the spike prime hub
+    To use the Lights module add the following import statement to your project:
+    
+    from Lights import Lights
+    
+    All functions in the module should be called inside the lights module as a prefix like so:
+    
+    Lights.pixel_set()
     '''
     def __init__(self):
         self.pixel_lut = [9,11,6,1,14,10,19,8,0,26,23,18,3,2,24,21,20,15,13,25,22,7,17,12,38,27,28,29,39,40,41,42,43,44,45,46,47]
@@ -20,7 +26,6 @@ class Lights():
         self.display_on()
         self.power_led(0,0,1)
 
-    #can't get this one to seem to work on its own but understand its concept
     def set(self, led, value = 256*256-1):
         '''
         set(led, value=256*256-1)
@@ -102,22 +107,24 @@ class Lights():
         time.sleep_us(1)
         self.tlcLat.off()
     
-    #check to see what the purpose of this is and what is the major difference between it and timer on
     def display_on(self):
         '''
         display_on()
         This function initialize power and properties of the LED configuration
+        
+        Return: Physical output on hub of color output on power LED
         '''
         self.tlcLat.off()
         for i in range(2):
             self.latch_ctrl(0xff, 0, 0x1fffff, 0x11)
         self.timer_init()
         
-    #check to see what purpose of this function is and why it is needed separately than display_on
     def start(self):
         '''
         start()
         This function turns the ble, power, and battery leds on
+        
+        Return: Physical output on hub of color output on battery, power, and ble LED
         '''
         self.pixel_set(25)
         self.pixel_set(26)
@@ -133,6 +140,8 @@ class Lights():
             x (integer) = left and right on the matrix
             y (integer) = up and down on the matrix
             b (integer) = brightness
+            
+        Return: Physical output on hub of color output on matrix LED
         '''
         self.pixel_set(y*5+x, self.fullBrightness*b)
         self.display_update()
@@ -147,6 +156,8 @@ class Lights():
             g (bool) = green 
             b (bool) = blue
             
+        Return: Physical output on hub of color output on ble LED
+        
         Note:
             Bools written as integer values of 0 and 1
         '''
@@ -164,7 +175,9 @@ class Lights():
             r (bool) = red
             g (bool) = green 
             b (bool) = blue
-            
+        
+        Return: Physical output on hub of color output on power LED
+        
         Note:
             Bools written as integer values of 0 and 1
         '''
@@ -177,6 +190,7 @@ class Lights():
         self.pixel_set(33, self.fullBrightness*b)
         self.display_update()
 
+    #this is currently turning on the center as well as the power led 
     def battery_led(self, r=1, g=0, b=0):
         '''
         battery_led(r, g, b)
@@ -187,6 +201,8 @@ class Lights():
             g (bool) = green 
             b (bool) = blue
             
+        Return: Physical output on hub of color output on battery LED
+        
         Note:
             Bools written as integer values of 0 and 1
         
@@ -195,13 +211,3 @@ class Lights():
         self.pixel_set(35, self.fullBrightness*g)
         self.pixel_set(36, self.fullBrightness*b)
         self.display_update()
-        
-    #this is testing only comment out later 
-    def full_test(self):
-        for i in range(48):
-            for b in range(0,self.fullBrightness,128):
-                self.pixel_set(i,b)
-                self.display_update()
-                time.sleep(0.001)
-            time.sleep(0.1)
-            if i: self.pixel_set(i-1,0)
